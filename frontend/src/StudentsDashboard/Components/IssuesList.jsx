f12448de957afa0c858984482a35123c8d9c0144
blaisedavis21 <blaisedavis207@gmail.com>
2025-03-19 16:55:41 +0300

    added issue list to components

--- frontend/src/StudentsDashboard/Components/IssuesList.jsx
+++ frontend/src/StudentsDashboard/Components/IssuesList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./IssuesList.css";
 
 const IssuesList = () => ({issues, addIssue}) => {
     const [searchIssue, setsearchIssue] = useState("");
