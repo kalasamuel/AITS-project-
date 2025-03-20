5ed8ae9693dfcfbc4cc03a29975b4deaeabc0b18
Kala Samuel <kalasamuel79@gmail.com>
2025-03-20 18:20:17 +0300

    added views with its urls in issues app

--- backend/issues/admin.py
+++ backend/issues/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Issue, Notification
+from .models import Issue, Notification, Course, Assignment, Enrollment
 
 # Register your models here.
 admin.site.register(Issue)
 admin.site.register(Notification)
+admin.site.register(Course)
+admin.site.register(Assignment)
+admin.site.register(Enrollment)
