e616ae9f80a0246e8b3e090e9d5a950b919ecd28
Kenaz79 <blessedkentacion@gmail.com>
2025-03-25 12:48:04 +0300

    Changed assigned issues

---
+++ frontend/src/LecturerDashboard/Pages/AssignedIssues.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function IssueSubmission() {
+    return (
+        <div>
+            <h1>Assigned Issues</h1>
+            <p>Welcome to the Assigned Issues page!</p>
+        </div>
+    );
+}
+export default IssueSubmission;
