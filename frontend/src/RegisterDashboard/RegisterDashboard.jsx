0701beb5c4fc0fe61b1b8625e289d8bbbe37739a
AHURIRA GRACE <graceahurira3@gmail.com>
2025-04-03 04:47:42 -0700

    y

---
+++ frontend/src/RegisterDashboard/RegisterDashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import useState from "react";
+import "./RegisterDashboard.css";
+
+const RegisterDashboard = () => {
+  return (
+    <div className="register-dashboard-container">
+      <h1>Register Dashboard</h1>
+      <p>Welcome to the Register Dashboard!</p>
+      <p>This is where you can manage your registration details.</p>
+      {/* Add more content or components as needed */}
+    </div>
+  );
+}
+export default RegisterDashboard;
