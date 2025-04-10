87e812064189e0f03d24b016537fa0957ea12150
Kala Samuel <kalasamuel79@gmail.com>
2025-04-10 18:26:32 +0300

    modified Navbar components

--- frontend/src/StudentsDashboard/Components/NavBar.jsx
+++ frontend/src/StudentsDashboard/Components/NavBar.jsx
@@ -1,27 +1,32 @@
+// NavBar.jsx
 import React from 'react';
 import './NavBar.css';
 import { useNavigate } from 'react-router-dom';
+import { MdMenu } from 'react-icons/md'; // Import the menu icon
 
-function NavBar({ setIsAuthenticated, profilePic }) {
+function NavBar({ profilePic, toggleSidebar }) {
     const navigate = useNavigate();
 
     const handleLogout = () => {
-        localStorage.clear()
-        navigate('/welcome'); 
+        localStorage.clear();
+        navigate('/welcome');
     };
 
     return (
         <nav className="navbar">
+            <button className="hamburger-button" onClick={toggleSidebar}>
+                <MdMenu size={24} />
+            </button>
             <div className="navbar-content">
                 <h1 className="aits">ACADEMIC ISSUE TRACKING SYSTEM</h1>
             </div>
             <div className="navbar-profile">
-            {profilePic ? (
-          <img src={profilePic} alt="Profile" className="navbar-img" />
-        ) : (
-          <span className="navbar-placeholder">No Profile Pic</span>
-        )}
-        <button className="logout-button" onClick={handleLogout}>
+                {profilePic ? (
+                    <img src={profilePic} alt="Profile" className="navbar-img" />
+                ) : (
+                    <span className="navbar-placeholder">No Profile Pic</span>
+                )}
+                <button className="logout-button" onClick={handleLogout}>
                     Log Out
                 </button>
             </div>
