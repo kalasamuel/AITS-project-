47d327604b63fa139271111be693b68e86d79091
Kala Samuel <kalasamuel79@gmail.com>
2025-04-23 09:09:20 +0300

    updated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,20 +9,18 @@ import RegistrarRoutes from "./Routes/RegistrarRoutes.jsx";
 import OtpVerification from "./Welcome/OtpVerification.jsx";
 import './styles.css';
 import useIsAuthenticated from '../hooks/useIsAuthenticated.js';
+import DashboardLayout from './StudentsDashboard/Components/DashboardLayout.jsx';
 import axios from 'axios';
-
-console.log("✅ App is rendering");
 
 const token = localStorage.getItem('access_token');
 if (token) {
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 
-
 function App() {
     const { isAuthenticated, userRole } = useIsAuthenticated();
 
-    console.log("Auth Check:", {isAuthenticated, userRole});
+    console.log("Auth Check:", { isAuthenticated, userRole });
 
     const [profilePic, setProfilePic] = useState(null);
     const location = useLocation();
@@ -34,73 +32,69 @@ function App() {
 
     return (
         <div>
-            {isAuthenticated && location.pathname !== '/welcome' && (
-                <NavBar profilePic={profilePic} toggleSidebar={toggleSidebar} />
-            )}
-            <div className="main-layout">
-                {isAuthenticated && location.pathname !== '/welcome' && (
-                    <SideBar userType={userRole} isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-                )}
-                <div className="main-content">
-                    <div className="content">
-                        <Routes>
-
-                            <Route path="/test" element={<h1>Hello from test route</h1>}/>
-
-                            <Route path="/welcome" element={<Welcome />} />
-                            <Route path="/otp-verification" element={<OtpVerification />} />
-                            <Route
-                                path="/"
-                                element={
-                                    isAuthenticated ? (
-                                        userRole === "student" ? (
-                                            <Navigate to="/student/home" />
-                                        ) : userRole === "lecturer" ? (
-                                            <Navigate to="/lecturer/home" />
-                                        ) : userRole === "registrar" ? (
-                                            <Navigate to="/registrar/home" />
-                                        ) : (
-                                            <Navigate to="/welcome" />
-                                        )
+            {isAuthenticated && location.pathname !== '/welcome' ? (
+                <DashboardLayout profilePic={profilePic} toggleSidebar={toggleSidebar}>
+                    <Routes>
+                        <Route path="/test" element={<h1>Hello from test route</h1>} />
+                        <Route path="/welcome" element={<Welcome />} />
+                        <Route path="/otp-verification" element={<OtpVerification />} />
+                        <Route
+                            path="/"
+                            element={
+                                isAuthenticated ? (
+                                    userRole === "student" ? (
+                                        <Navigate to="/student/home" />
+                                    ) : userRole === "lecturer" ? (
+                                        <Navigate to="/lecturer/home" />
+                                    ) : userRole === "registrar" ? (
+                                        <Navigate to="/registrar/home" />
                                     ) : (
                                         <Navigate to="/welcome" />
                                     )
-                                }
-                            />
-                            <Route
-                                path="/student/*"
-                                element={
-                                    <StudentRoutes
-                                        isAuthenticated={isAuthenticated}
-                                        profilePic={profilePic}
-                                        setProfilePic={setProfilePic}
-                                    />
-                                }
-                            />
-                            <Route
-                                path="/lecturer/*"
-                                element={
-                                    <LecturerRoutes
-                                        isAuthenticated={isAuthenticated}
-                                        profilePic={profilePic}
-                                        setProfilePic={setProfilePic}
-                                    />
-                                }
-                            />
-                            <Route
-                                path="/registrar/*"
-                                element={
-                                    <RegistrarRoutes
-                                        isAuthenticated={true}
-                                        profilePic={profilePic}
-                                        setProfilePic={setProfilePic}
-                                    />
-                                }
-                            />
-                        </Routes>
-                    </div>
-                </div>
-            </div>
+                                ) : (
+                                    <Navigate to="/welcome" />
+                                )
+                            }
+                        />
+                        <Route
+                            path="/student/*"
+                            element={
+                                <StudentRoutes
+                                    isAuthenticated={isAuthenticated}
+                                    profilePic={profilePic}
+                                    setProfilePic={setProfilePic}
+                                />
+                            }
+                        />
+                        <Route
+                            path="/lecturer/*"
+                            element={
+                                <LecturerRoutes
+                                    isAuthenticated={isAuthenticated}
+                                    profilePic={profilePic}
+                                    setProfilePic={setProfilePic}
+                                />
+                            }
+                        />
+                        <Route
+                            path="/registrar/*"
+                            element={
+                                <RegistrarRoutes
+                                    isAuthenticated={true}
+                                    profilePic={profilePic}
+                                    setProfilePic={setProfilePic}
+                                />
+                            }
+                        />
+                    </Routes>
+                </DashboardLayout>
+            ) : (
+                <Routes>
+                    <Route path="/welcome" element={<Welcome />} />
+                    <Route path="/otp-verification" element={<OtpVerification />} />
+                    <Route path="*" element={<Navigate to="/welcome" />} />
+                </Routes>
+            )}
         </div>
     );
 }
