101fc6e1d768bad3a00d373e2f623b527224ef56
Gift038 <164305468+Gift038@users.noreply.github.com>
2025-04-25 07:24:24 +0300

    Updated settings.py by adding the local host link

--- backend/aits_project/settings.py
+++ backend/aits_project/settings.py
@@ -42,6 +42,7 @@ DEBUG = True
 CORS_ALLOWED_ORIGINS = [ 
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5174",
     "https://group-t-aits.netlify.app", #our frontend deployment using netlify
 ]
 
