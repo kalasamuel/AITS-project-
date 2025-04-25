c10a25b1df9ef2858ea40c75e894565a27970d28
Kala Samuel <kalasamuel79@gmail.com>
2025-04-25 06:17:47 +0300

    Updated the settings.py by adding static_root

--- backend/aits_project/settings.py
+++ backend/aits_project/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import environ # from the django-environ package installed
 from datetime import timedelta
+import os
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 env = environ.Env(DEBUG=(bool,False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
